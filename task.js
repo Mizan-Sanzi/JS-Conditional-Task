bae1a11b2f8b923598ff4d89e4feb49f7d728d0c
mizansanzi <pleasemailmizan@gmail.com>
2024-09-04 12:33:10 +0600

    added first task

---
+++ task.js
@@ -0,0 +1,14 @@
+// task 1
+
+/* Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk */
+
+var totalPay = 500;
+var burger;
+if (burger>totalPay){
+    console.log("Congratulations! You have got a free coke!");
+}
+else{
+    console.log("Sorry! You have no free offer! Please pay 30 tk. for coke.");
+}
